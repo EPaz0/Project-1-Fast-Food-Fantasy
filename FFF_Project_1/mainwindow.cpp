14633751c711895f80c3af02dfa028f3b6097f4b
42-Max-42 <Maximprosser133@yahoo.com>
2022-03-06 21:33:06 -0800

    MainWindow and Signin Ui/Classes

---
+++ FFF_Project_1/mainwindow.cpp
@@ -0,0 +1,40 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->actionUpdate_List->setDisabled(true);
+    ui->actionLog_out->setDisabled(true);
+    MainWindow::setToolTipDuration(500);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_actionLog_in_triggered()
+{
+    hide();
+    signin = new Signin(this);
+    signin->show();
+}
+
+void MainWindow::Admin()
+{
+    ui->actionLog_in->setDisabled(true);
+    ui->actionLog_out->setDisabled(false);
+    ui->actionUpdate_List->setDisabled(false);
+}
+
+void MainWindow::on_actionLog_out_triggered()
+{
+    ui->actionLog_in->setDisabled(false);
+    ui->actionLog_out->setDisabled(true);
+    ui->actionUpdate_List->setDisabled(true);
+}
+
