792c658d0287e473c76e69c56544502703a012d5
Van Pham <thyvan1412@gmail.com>
2022-03-11 17:46:37 -0800

    updated closesttrip to displayed menu, price, add remove from cart

--- FFF_Project_1/closesttrip.cpp
+++ FFF_Project_1/closesttrip.cpp
@@ -10,45 +10,160 @@ ClosestTrip::ClosestTrip(QWidget *parent) :
     connect(this,SIGNAL(Admin()), parent, SLOT(Admin()));
     connect(this,SIGNAL(Admin()), parent, SLOT(show()));
 
-    // import the table already created
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
     QString dbPath = QCoreApplication::applicationDirPath() + "/restaurant.sqlite";
     db.setDatabaseName(dbPath);
-
     if (!db.open())
     {
         qDebug() << "problem opening database";
     }
 
-    QString item;
 
-    // test sql - display info on closest trip list, from closest to furthest
-    QSqlQuery qry;
+    QString restaurant;
+
+    // display info on closest trip list, order from closest to furthest
     qry.prepare("SELECT restaurantName FROM restaurantList ORDER BY distancetoSaddleback");
 
     if (qry.exec())
     {
         while (qry.next())
         {
-            item = qry.value(0).toString();
-             ui->listWidget->addItem(item);
+            restaurant = qry.value(0).toString();
+             ui->listWidget->addItem(restaurant);  // add each restaurant to the widget
         }
     }
-
-    db.close();
-    const QString connectionName = db.connectionName();
-    db = QSqlDatabase();
-    QSqlDatabase::removeDatabase(connectionName);
 }
 
 ClosestTrip::~ClosestTrip()
 {
+    db.close();
+    const QString connectionName = db.connectionName();
+    db = QSqlDatabase();
+    QSqlDatabase::removeDatabase(connectionName);
     delete ui;
 }
+
 
 void ClosestTrip::on_pushButton_3_clicked()
 {
     hide();
     emit Admin();
+}
+
+
+void ClosestTrip::on_pushButton_4_clicked()
+{
+    ui->listWidget_menu->clear();
+    ui->listWidget_price->clear();
+    ui->listWidget_cartItem->clear();
+    ui->listWidget_cartPrice->clear();
+
+
+    QString restaurantname = ui->listWidget->currentItem()->text();
+    ui->name->setText(restaurantname); // display restaurant  name
+
+    // change to sql query format when string contains apostrophe (need to add one more ' to the query)
+    if (restaurantname == "MacDonald's")
+        restaurantname = "MacDonald''s";
+    if (restaurantname == "Domino's Pizza")
+        restaurantname = "Domino''s Pizza";
+    if (restaurantname == "Wendy's")
+        restaurantname = "Wendy''s";
+    if (restaurantname == "Papa John's Pizza")
+        restaurantname = "Papa John''s Pizza";
+
+
+    int restaurantId;
+    // get restaurant's id number for the restaurant name, use restaurantList table
+    QString stringQry = "SELECT id FROM restaurantList WHERE restaurantName = '" + restaurantname + "'";
+    qry.prepare(stringQry);
+    if (qry.exec())
+    {
+        while (qry.next())
+        {
+            restaurantId = qry.value(0).toInt();
+        }
+    }
+
+
+    // display menu and price
+    QString menuItem;
+    QString price;
+    stringQry = "SELECT * FROM menu WHERE restaurantID = " + QString::number(restaurantId);
+    qry.prepare(stringQry);  // pass the stringQry to the SQL query
+    if (qry.exec())
+    {
+        while (qry.next())
+        {
+            menuItem = qry.value(1).toString();   // in here, 1 specifies column 2 of menu table (item)
+            ui->listWidget_menu->addItem(menuItem); // add menu item to the listWidget_menu
+            price = qry.value(2).toString();    // in here, 2 specifies column 3 of menu table (price)
+            ui->listWidget_price->addItem("$" + price);  // add price to the listWidget_price
+        }
+    }
+
+}
+
+
+// this is the action set when "add" button is clicked
+// purpose: add menuitem to cart
+void ClosestTrip::on_pushButton_clicked()
+{
+    QString currentItem = ui->listWidget_menu->currentItem()->text();
+    QListWidgetItem* item = new QListWidgetItem(currentItem);
+    ui->listWidget_cartItem->addItem(item);   // add menu item name to cart widget
+
+    QString restaurantname = ui->listWidget->currentItem()->text();
+    // change to sql query format when string contains apostrophe
+    if (restaurantname == "MacDonald's")
+        restaurantname = "MacDonald''s";
+    if (restaurantname == "Domino's Pizza")
+        restaurantname = "Domino''s Pizza";
+    if (restaurantname == "Wendy's")
+        restaurantname = "Wendy''s";
+    if (restaurantname == "Papa John's Pizza")
+        restaurantname = "Papa John''s Pizza";
+
+    int restaurantId;
+    // get restaurant's id number from restaurant name
+    QString stringQry = "SELECT id FROM restaurantList WHERE restaurantName = '" + restaurantname + "'";
+    qry.prepare(stringQry);
+    if (qry.exec())
+    {
+        while (qry.next())
+        {
+            restaurantId = qry.value(0).toInt();
+        }
+    }
+
+    float price;  // get the price, having menuitem and restaurant id
+    stringQry = "SELECT price FROM menu WHERE restaurantID = '" + QString::number(restaurantId) + "'" + " AND item = '" + currentItem + "'";
+    qry.prepare(stringQry);
+    if (qry.exec())
+    {
+        while (qry.next())
+        {
+            price = qry.value(0).toFloat();
+        }
+    }
+
+
+    ui->listWidget_cartPrice->addItem(QString::number(price));   // add price to cart widget
+}
+
+
+// this is the action set when "remove" button is clicked
+// purpose: delete menuitem from cart
+void ClosestTrip::on_pushButton_2_clicked()
+{
+    QList<QListWidgetItem*> items = ui->listWidget_cartItem->selectedItems();
+
+    foreach(QListWidgetItem* item, items){
+        int row = ui->listWidget_cartItem->row(item);
+        QListWidgetItem* itemPrice = ui->listWidget_cartPrice->item(row);
+        ui->listWidget_cartItem->removeItemWidget(item);
+        ui->listWidget_cartPrice->removeItemWidget(itemPrice);
+        delete item;
+        delete itemPrice;
+    }
 }
 
--- build-FFF_Project_1-Desktop_Qt_6_3_0_MinGW_64_bit-Debug/debug/moc_closesttrip.cpp
+++ build-FFF_Project_1-Desktop_Qt_6_3_0_MinGW_64_bit-Debug/debug/moc_closesttrip.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_ClosestTrip_t {
-    const uint offsetsAndSize[8];
-    char stringdata0[43];
+    const uint offsetsAndSize[14];
+    char stringdata0[113];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(offsetof(qt_meta_stringdata_ClosestTrip_t, stringdata0) + ofs), len 
@@ -32,10 +32,15 @@ static const qt_meta_stringdata_ClosestTrip_t qt_meta_stringdata_ClosestTrip = {
 QT_MOC_LITERAL(0, 11), // "ClosestTrip"
 QT_MOC_LITERAL(12, 5), // "Admin"
 QT_MOC_LITERAL(18, 0), // ""
-QT_MOC_LITERAL(19, 23) // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(19, 23), // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(43, 23), // "on_pushButton_4_clicked"
+QT_MOC_LITERAL(67, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(89, 23) // "on_pushButton_2_clicked"
 
     },
-    "ClosestTrip\0Admin\0\0on_pushButton_3_clicked"
+    "ClosestTrip\0Admin\0\0on_pushButton_3_clicked\0"
+    "on_pushButton_4_clicked\0on_pushButton_clicked\0"
+    "on_pushButton_2_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -45,7 +50,7 @@ static const uint qt_meta_data_ClosestTrip[] = {
       10,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -53,15 +58,21 @@ static const uint qt_meta_data_ClosestTrip[] = {
        1,       // signalCount
 
  // signals: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   26,    2, 0x06,    1 /* Public */,
+       1,    0,   44,    2, 0x06,    1 /* Public */,
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       3,    0,   27,    2, 0x08,    2 /* Private */,
+       3,    0,   45,    2, 0x08,    2 /* Private */,
+       4,    0,   46,    2, 0x08,    3 /* Private */,
+       5,    0,   47,    2, 0x08,    4 /* Private */,
+       6,    0,   48,    2, 0x08,    5 /* Private */,
 
  // signals: parameters
     QMetaType::Void,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
 
        0        // eod
@@ -75,6 +86,9 @@ void ClosestTrip::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id,
         switch (_id) {
         case 0: _t->Admin(); break;
         case 1: _t->on_pushButton_3_clicked(); break;
+        case 2: _t->on_pushButton_4_clicked(); break;
+        case 3: _t->on_pushButton_clicked(); break;
+        case 4: _t->on_pushButton_2_clicked(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -98,7 +112,7 @@ const QMetaObject ClosestTrip::staticMetaObject = { {
     nullptr,
 qt_incomplete_metaTypeArray<qt_meta_stringdata_ClosestTrip_t
 , QtPrivate::TypeAndForceComplete<ClosestTrip, std::true_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
-, QtPrivate::TypeAndForceComplete<void, std::false_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
 
 
 >,
@@ -125,13 +139,13 @@ int ClosestTrip::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 5)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 5;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 5)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 2;
+        _id -= 5;
     }
     return _id;
 }
