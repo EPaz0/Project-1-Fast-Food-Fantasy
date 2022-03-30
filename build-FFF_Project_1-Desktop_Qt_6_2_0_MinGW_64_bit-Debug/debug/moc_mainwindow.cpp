63fcbad88642bd75fa74da52a8a673c32a6eabf5
Cheng Qian <qiancheng0712@gmail.com>
2022-03-29 21:27:11 -0700

    Implemented the trip option for all twelve restaurants

--- build-FFF_Project_1-Desktop_Qt_6_2_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
+++ build-FFF_Project_1-Desktop_Qt_6_2_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    const uint offsetsAndSize[40];
-    char stringdata0[444];
+    const uint offsetsAndSize[42];
+    char stringdata0[476];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs), len 
@@ -48,7 +48,8 @@ QT_MOC_LITERAL(315, 25), // "on_deleteMenuItem_clicked"
 QT_MOC_LITERAL(341, 18), // "on_addMenu_clicked"
 QT_MOC_LITERAL(360, 20), // "on_SubmitNew_clicked"
 QT_MOC_LITERAL(381, 31), // "on_listWidget_price_itemClicked"
-QT_MOC_LITERAL(413, 30) // "on_actionUpdate_List_triggered"
+QT_MOC_LITERAL(413, 30), // "on_actionUpdate_List_triggered"
+QT_MOC_LITERAL(444, 31) // "on_actionVisit_All_12_triggered"
 
     },
     "MainWindow\0isAdmin\0\0on_actionLog_in_triggered\0"
@@ -63,7 +64,8 @@ QT_MOC_LITERAL(413, 30) // "on_actionUpdate_List_triggered"
     "on_deleteMenuItem_clicked\0on_addMenu_clicked\0"
     "on_SubmitNew_clicked\0"
     "on_listWidget_price_itemClicked\0"
-    "on_actionUpdate_List_triggered"
+    "on_actionUpdate_List_triggered\0"
+    "on_actionVisit_All_12_triggered"
 };
 #undef QT_MOC_LITERAL
 
@@ -73,7 +75,7 @@ static const uint qt_meta_data_MainWindow[] = {
       10,       // revision
        0,       // classname
        0,    0, // classinfo
-      16,   14, // methods
+      17,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -81,24 +83,25 @@ static const uint qt_meta_data_MainWindow[] = {
        1,       // signalCount
 
  // signals: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,  110,    2, 0x06,    1 /* Public */,
+       1,    0,  116,    2, 0x06,    1 /* Public */,
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       3,    0,  111,    2, 0x08,    2 /* Private */,
-       4,    0,  112,    2, 0x08,    3 /* Private */,
-       5,    0,  113,    2, 0x08,    4 /* Private */,
-       6,    0,  114,    2, 0x08,    5 /* Private */,
-       7,    0,  115,    2, 0x08,    6 /* Private */,
-       8,    1,  116,    2, 0x08,    7 /* Private */,
-      11,    0,  119,    2, 0x08,    9 /* Private */,
-      12,    0,  120,    2, 0x08,   10 /* Private */,
-      13,    0,  121,    2, 0x08,   11 /* Private */,
-      14,    1,  122,    2, 0x08,   12 /* Private */,
-      15,    0,  125,    2, 0x08,   14 /* Private */,
-      16,    0,  126,    2, 0x08,   15 /* Private */,
-      17,    0,  127,    2, 0x08,   16 /* Private */,
-      18,    1,  128,    2, 0x08,   17 /* Private */,
-      19,    0,  131,    2, 0x08,   19 /* Private */,
+       3,    0,  117,    2, 0x08,    2 /* Private */,
+       4,    0,  118,    2, 0x08,    3 /* Private */,
+       5,    0,  119,    2, 0x08,    4 /* Private */,
+       6,    0,  120,    2, 0x08,    5 /* Private */,
+       7,    0,  121,    2, 0x08,    6 /* Private */,
+       8,    1,  122,    2, 0x08,    7 /* Private */,
+      11,    0,  125,    2, 0x08,    9 /* Private */,
+      12,    0,  126,    2, 0x08,   10 /* Private */,
+      13,    0,  127,    2, 0x08,   11 /* Private */,
+      14,    1,  128,    2, 0x08,   12 /* Private */,
+      15,    0,  131,    2, 0x08,   14 /* Private */,
+      16,    0,  132,    2, 0x08,   15 /* Private */,
+      17,    0,  133,    2, 0x08,   16 /* Private */,
+      18,    1,  134,    2, 0x08,   17 /* Private */,
+      19,    0,  137,    2, 0x08,   19 /* Private */,
+      20,    0,  138,    2, 0x08,   20 /* Private */,
 
  // signals: parameters
     QMetaType::Void,
@@ -118,6 +121,7 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void, 0x80000000 | 9,   10,
+    QMetaType::Void,
     QMetaType::Void,
 
        0        // eod
@@ -145,6 +149,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 13: _t->on_SubmitNew_clicked(); break;
         case 14: _t->on_listWidget_price_itemClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
         case 15: _t->on_actionUpdate_List_triggered(); break;
+        case 16: _t->on_actionVisit_All_12_triggered(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -167,7 +172,7 @@ const QMetaObject MainWindow::staticMetaObject = { {
     nullptr,
 qt_incomplete_metaTypeArray<qt_meta_stringdata_MainWindow_t
 , QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
-, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
 
 
 >,
@@ -194,13 +199,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 16)
+        if (_id < 17)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 16;
+        _id -= 17;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 16)
+        if (_id < 17)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 16;
+        _id -= 17;
     }
     return _id;
 }
